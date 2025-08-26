95301582f6c06ff4d24d0b1baa5dc7873401f25d
RebecaTI <mebuffbeka@gmail.com>
2025-08-26 10:56:03 -0300

    add initial server and response handling logic

---
+++ 01-node-http/server.js
@@ -0,0 +1,27 @@
+const http = require('node:http')
+
+const server = http.createServer((request, response) => {
+  const path = request.url
+
+  switch (path) {
+    case '/':
+      response.writeHead(200)
+      response.write('Vc está na pagina inicial!')
+      break;
+    case '/artigos':
+      response.writeHead(200)
+      response.write('Vc está na pagina de artigos!')
+      break;
+    default:
+      response.writeHead(404)
+      response.write('Caminho não encontrado!')
+      break;
+  }
+  response.end()
+})
+
+const PORT = 3000
+
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
